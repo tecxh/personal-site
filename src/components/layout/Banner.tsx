41bb95ba4bea3b2e1bbc41c3bf14d9b422b1c401
tecxh <techno.zelda@gmail.com>
2025-08-26 13:47:59 -0400

    extract banner

---
+++ src/components/layout/Banner.tsx
@@ -0,0 +1,14 @@
+export const Banner = () => {
+  return (
+    <header role="banner">
+      <h1>alecx moritz</h1>
+      <h2>accessible software engineer</h2>
+      <nav>
+        <ul>
+          <li>site map</li>
+          <li>to top</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
