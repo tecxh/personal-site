8f306db530f7efa4f49ac1d8b385405ebec7d229
tecxh <techno.zelda@gmail.com>
2025-09-06 01:35:21 -0400

    exo. section adjustments

--- src/components/ExperienceSubSection.tsx
+++ src/components/ExperienceSubSection.tsx
@@ -5,22 +5,22 @@ export const ExperienceSubSection = () => {
         `
     }
     return (
-        <div className="mb-60">
-            <h3 className="text-5xl mb-10 text-fuchsia-400">Experience</h3>
-            <div className="flex flex-col-reverse sm:flex-row gap-10">
-                <div className="sm:w-120">
-                    <ul className="text-2xl">
+        <div className="mb-10 lg:mb-30">
+            <h3 className="text-2xl lg:text-5xl mb-2 lg:mb-10 text-fuchsia-400">Experience</h3>
+            <div className="flex flex-col-reverse lg:flex-row gap-4 lg:gap-10">
+                <div className="lg:w-120">
+                    <ul className="text-md lg:text-2xl">
                         <li className="text-fuchsia-300">2019 - 2025, Software Engineer II, Augeo</li>
                         <p className="text-sm">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam commodi illum deserunt quod, ad voluptas blanditiis! Suscipit voluptates, inventore, reiciendis explicabo ullam qui velit libero, eaque mollitia repellendus facere nisi?</p>
-                        <li className="text-fuchsia-300 mt-12">2018 - 2019, Instructor, 1150 Academy</li>
+                        <li className="text-fuchsia-300 mt-4 lg:mt-12">2018 - 2019, Instructor, 1150 Academy</li>
                         <p className="text-sm">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam nobis eum voluptatibus, ipsum iste deserunt similique consequuntur quos rerum distinctio voluptate, nam impedit quibusdam fugit. Quos neque quam repellendus possimus!</p>
                     </ul>
                 </div>
                 <div className="flex-1">
-                    <p className="text-2xl">
+                    <p className="text-1xl lg:text-2xl">
                         {text.mainCopy}
                     </p>
-                    <p className="text-2xl mt-2">To view my wider accomplishments, my resume is available <a className="text-fuchsia-300">here</a>.</p>
+                    <p className="text-1xl lg:text-2xl mt-2">To view my wider accomplishments, my resume is available <a className="text-fuchsia-300">here</a>.</p>
                 </div>
             </div>
         </div>
